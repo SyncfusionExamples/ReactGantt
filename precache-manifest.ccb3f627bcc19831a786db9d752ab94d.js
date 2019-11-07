72405676a5b85e0acd998469750bd9a968f9fdf9
Manikandan  Manikam <manikandanmanikam@syncfusion.com>
2019-11-07 12:26:41 +0530

    Updates

---
+++ precache-manifest.ccb3f627bcc19831a786db9d752ab94d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5b8ecbd1f2cfe51dfece152e8e4c0e",
+    "url": "/ReactGantt/index.html"
+  },
+  {
+    "revision": "c31e82b8203afb583c05",
+    "url": "/ReactGantt/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a68696196a08a4e2c990",
+    "url": "/ReactGantt/static/js/2.f427ddad.chunk.js"
+  },
+  {
+    "revision": "c31e82b8203afb583c05",
+    "url": "/ReactGantt/static/js/main.3c15f276.chunk.js"
+  },
+  {
+    "revision": "c2997380afe8ce1b01e8",
+    "url": "/ReactGantt/static/js/runtime-main.8f4622c0.js"
+  }
+]);
